e9220b3ba1602dacd97ed58c840d4cfa92c543bf
南宫乘风 <1794748404@qq.com>
2023-03-16 16:14:04 +0800

    增加consul自动发现注解

--- consul-Provider/src/main/java/com/example/consulprovider/ConsulProviderApplication.java
+++ consul-Provider/src/main/java/com/example/consulprovider/ConsulProviderApplication.java
@@ -2,8 +2,9 @@ package com.example.consulprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsulProviderApplication {
 
     public static void main(String[] args) {
